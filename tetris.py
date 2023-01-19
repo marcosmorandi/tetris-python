45cba16093a24d5b54cd86ca7096de82e962a9c2
marcosmorandi <marcosamorandi@gmail.com>
2023-01-19 17:31:09 -0300

    Função para criar novo formato.

--- tetris.py
+++ tetris.py
@@ -215,8 +215,20 @@ def clear_rows():
     return inc
 
 
-def draw_next_shape():
-    pass
+def draw_next_shape(shape, surface):
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Next Shape', 1, (255,255,255))
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height / 2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i. line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color, (sx + j * block_size, sy + i * block_size, block_size, block_size), 0)
+    
+    surface.blit(label, (sx + 10, sy - 30))
 
 
 def draw_window(surface, grid, score = 0, last_score = 0):
