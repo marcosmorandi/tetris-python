2acc6e7a2ae1667e8f86bef44fab4fb525e7d7c7
marcosmorandi <marcosamorandi@gmail.com>
2023-01-19 09:36:33 -0300

    Criando a função para limpar linhas.

--- tetris.py
+++ tetris.py
@@ -194,7 +194,25 @@ def draw_grid(surface, grid):
 
 
 def clear_rows():
-    pass
+    inc = 0
+    for i in range(len(grid)-1, -1, -1):
+        row = grid[i]
+        if (0,0,0) not in row:
+            inc += 1
+            ind = 1
+            for j in range(len(row)):
+                try:
+                    del locked[(j,i)]
+                except:
+                    continue
+
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[i])[::-1]
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
+    return inc
 
 
 def draw_next_shape():
